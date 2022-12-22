d306f6856a2bc1d6878c0e5ef408131d9c2e6587
nihalkumar33 <110326182+nihalkumar33@users.noreply.github.com>
2022-12-23 00:52:23 +0530

    Update MergingArrays.c

--- MergingArrays.c
+++ MergingArrays.c
@@ -3,8 +3,10 @@
 
 void MergingArrays(int *A, int *B, int *p, int l1, int l2)
 {
+    int CheckEnd=-1;       //Tells that which array have reached to it's end.
     int i=0, j=0, k=0, var;       //i is for A, j is for B, k is for SortedArray, var is for comparing
-    while (i<l1 && j<l2)
+    
+    while (i<l1 && j<l2)        //Until one array is finished this will exe.
     {
         var = (*(A+i) == *(B+j))?(-1):((*(A+i)>*(B+j))?(1):(0));
         //var will have -1 if both nos are same
@@ -23,19 +25,45 @@ void MergingArrays(int *A, int *B, int *p, int l1, int l2)
         }
         else
         {
-            *(p+k) = *(A+i);
-            *(p+k+1) = *(B+j);
-            printf("%d %d Check the values: \n", *(p+k), *(A+i));
+            *(p+k) = *(A+i); k++;
+            *(p+k) = *(B+j);
             k++; i++; j++;
+        }
+
+        CheckEnd = (i==l1 && j==l2)?(0):((i==l1)?(1):((j==l2)?(2):(-1)));
+        //0: Both arrays have reached its end, 1: A has reached end, 2: B has reached it's end.
+    }
+
+    if (CheckEnd == 0)
+    {
+        int var;
+        var = (*(A+i)>*(B+j))?(*(A+i)):(*(B+j));
+        *(p+k) = var;
+    }
+
+    else if (CheckEnd == 1)
+    {
+        while (j<l2)
+        {
+            *(p+k) = *(B+j);
+            j++; k++;
+        }
+    }
+    else if (CheckEnd == 2)
+    {
+        while (i<l1)
+        {
+            *(p+k) = *(A+i);
+            i++; k++;
         }
     }
 }
 
 int main()
 {
-    int A[] = {1, 4, 6, 7, 16, 100};
-    int B[] = {5, 9, 12, 15, 20, 30};
-    int l1 = 6, l2 = 6;
+    int A[] = {2000};
+    int B[] = {0, 1000};
+    int l1 = 1, l2 = 2;
 
     int *p;
     p = (int *)malloc((l1 + l2)*sizeof(int));       //Creating space for Sorted Array.
