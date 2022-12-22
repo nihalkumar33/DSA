e23986e281085dfd91b3dff16152b142bc4d107e
Nihal <nihal7u@gmail.com>
2022-12-22 18:23:40 +0530

    A wrong commit

--- MergingArrays.c
+++ MergingArrays.c
@@ -4,27 +4,8 @@
 void MergingArrays(int *A, int *B, int *p, int l1, int l2)
 {
     int i=0, j=0, k=0, var;     //i is for A, j is for B, k is for SortedArray, var is for comparing
-    while (i<l1 || j<l2)
-    {   //We will check which of the list is smaller or the one which reaches to the end early.
-        int large;
-        large = (l1==l2)?(-1):((l1>l2)?(1):(0));    //1:l1>l2, 0: l1<l2, -1: l1=l2
-        
-        if (large == 1)       //If elements in A are remaning.
-        {
-            *(p+k) = *(A+i);
-            k++; i++;
-        }
-
-        else if (large == 0)        //If elements in B are remaning.
-        {
-            *(p+k) = *(B+j);
-            k++; j++;
-        }
-
-        else        //If both array's last elements are remaning.
-        {
-            
-        }
+    while (i<l1 && j<l2)
+    {   
         if (i != l1-1 && j != l2-1)     //This will execute until no list have reached to its end.
         {
             var = (*(A+i) == *(B+j))?(-1):((*(A+i)>*(B+j))?(1):(0));
@@ -44,8 +25,8 @@ void MergingArrays(int *A, int *B, int *p, int l1, int l2)
             }
             else
             {
-                *(p+k) = *(A+i);
-                *(p+k+1) = *(B+j);
+                *(p+k) = *(A+i); k++;
+                *(p+k) = *(B+j);
                 printf("%d %d Check the values: \n", *(p+k), *(A+i));
                 k++; i++; j++;
             }
